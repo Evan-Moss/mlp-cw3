ed9911ca00279eb1d8de062e14ad58d29ec00a65
Billy <s1664817@sms.ed.ac.uk>
2021-02-12 12:35:58 +0000

    Added legend to plot.

--- helper.py
+++ helper.py
@@ -1,12 +1,13 @@
 import matplotlib.pyplot as plt
 from IPython import display
-print(plt.get_backend())
 
+# Enables interactive mode so the graph can be continually updated.
 plt.ion()
 
 def plot(scores, mean_scores):
     display.clear_output(wait=True)
     display.display(plt.gcf())
+    
     plt.clf()
     plt.title('Training...')
     plt.xlabel('Number of Games')
@@ -16,6 +17,7 @@ def plot(scores, mean_scores):
     plt.ylim(ymin=0)
     plt.text(len(scores)-1, scores[-1], str(scores[-1]))
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
+    plt.legend(["Score", "Score Mean"], loc="upper left", fontsize="x-small")
     
     # Fixed a blocking issue - plot window was not updating.
     plt.draw()
